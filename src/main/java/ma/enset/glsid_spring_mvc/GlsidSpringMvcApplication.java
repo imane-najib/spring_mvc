867a731fdf56d61dcd52843dd2d0e7900f480d14
najib43imane <najibimane093@gmail.com>
2026-03-04 23:32:47 +0000

    git commit -m "Premier test"

--- src/main/java/ma/enset/glsid_spring_mvc/GlsidSpringMvcApplication.java
+++ src/main/java/ma/enset/glsid_spring_mvc/GlsidSpringMvcApplication.java
@@ -1,13 +1,48 @@
 package ma.enset.glsid_spring_mvc;
 
+import ma.enset.glsid_spring_mvc.entities.Product;
+import ma.enset.glsid_spring_mvc.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class GlsidSpringMvcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GlsidSpringMvcApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ProductRepository productRepository){
+            return args -> {
+               productRepository.save(Product
+                       .builder()
+                               .name("Computer")
+                               .price(1200)
+                               .quantity(11)
+                       .build());
+                productRepository.save(Product
+                        .builder()
+                        .name("Printer")
+                        .price(1200)
+                        .quantity(11)
+                        .build());
+                productRepository.save(Product
+                        .builder()
+                        .name("Smart Phone")
+                        .price(12000)
+                        .quantity(33)
+                        .build());
+
+                productRepository.findAll().forEach(
+                        p-> {
+                            System.out.println(p.toString());
+                        } );
+            };
     }
 
 }
