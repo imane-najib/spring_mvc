e7a3a3e33a0fee92e5766cbc260b76f59fbb65fb
najib43imane <najibimane093@gmail.com>
2026-03-04 20:50:07 +0000

    git commit -m "Initial commit"

---
+++ src/main/java/ma/enset/glsid_spring_mvc/GlsidSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.glsid_spring_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlsidSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlsidSpringMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/glsid_spring_mvc/GlsidSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.glsid_spring_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlsidSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
