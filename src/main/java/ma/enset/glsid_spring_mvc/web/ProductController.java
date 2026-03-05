acefbfef0e88696ab51652be3482cf012a4105f8
najib43imane <najibimane093@gmail.com>
2026-03-05 00:21:59 +0000

    git commit -m "L'implementation du page index avec la fonctionnalité de suppression"

---
+++ src/main/java/ma/enset/glsid_spring_mvc/web/ProductController.java
@@ -0,0 +1,35 @@
+package ma.enset.glsid_spring_mvc.web;
+
+
+import ma.enset.glsid_spring_mvc.entities.Product;
+import ma.enset.glsid_spring_mvc.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class ProductController {
+
+    @Autowired
+
+    private ProductRepository productRepository;
+
+    @GetMapping("/index")
+    public String index(Model model){
+        List<Product> products = productRepository.findAll();
+        model.addAttribute("productList",products);
+        return "products";
+    }
+
+    @GetMapping("/delete")
+    public String Delete(@RequestParam(name = "id") Long id){
+        productRepository.deleteById(id);
+        return "redirect:/index";
+    }
+
+
+}
