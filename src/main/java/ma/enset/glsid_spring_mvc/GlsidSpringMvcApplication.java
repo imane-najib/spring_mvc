ead86a728bea2d534d87373a64d87fb2ae2f3219
najib43imane <najibimane093@gmail.com>
2026-03-06 00:44:56 +0000

    git commit -m "La premiere implementation de la sécurité"

--- src/main/java/ma/enset/glsid_spring_mvc/GlsidSpringMvcApplication.java
+++ src/main/java/ma/enset/glsid_spring_mvc/GlsidSpringMvcApplication.java
@@ -9,7 +9,8 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class GlsidSpringMvcApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/ma/enset/glsid_spring_mvc/sec/SecurityConfig.java
@@ -0,0 +1,41 @@
+package ma.enset.glsid_spring_mvc.sec;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public InMemoryUserDetailsManager InMemoryUserDetailsManager(){
+        PasswordEncoder encoder = passwordEncoder();
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password(encoder.encode("1234")).roles("USER").build(),
+                User.withUsername("user2").password(encoder.encode("1234")).roles("USER").build(),
+                User.withUsername("admin").password(encoder.encode("1234")).roles("USER","ADMIN").build()
+        );
+    }
+    @Bean
+    public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception{
+
+         return http
+                 .formLogin(Customizer.withDefaults())
+                 .authorizeHttpRequests(ar->ar.requestMatchers("/index/**").hasRole("USER"))
+                 .authorizeHttpRequests(ar->ar.requestMatchers("/save**/**","/delete/**").hasRole("ADMIN"))
+                 .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+                 .build();
+    }
+}
