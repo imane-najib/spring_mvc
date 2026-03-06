41a760021e972289810d13f93f9c31298bebdfee
najib43imane <najibimane093@gmail.com>
2026-03-06 01:06:36 +0000

    git commit -m "Changement dans L'implementation de la sécurité"

--- src/main/java/ma/enset/glsid_spring_mvc/sec/SecurityConfig.java
+++ src/main/java/ma/enset/glsid_spring_mvc/sec/SecurityConfig.java
@@ -33,8 +33,9 @@ public class SecurityConfig {
 
          return http
                  .formLogin(Customizer.withDefaults())
-                 .authorizeHttpRequests(ar->ar.requestMatchers("/index/**").hasRole("USER"))
-                 .authorizeHttpRequests(ar->ar.requestMatchers("/save**/**","/delete/**").hasRole("ADMIN"))
+                 .authorizeHttpRequests(ar->ar.requestMatchers("/user/**").hasRole("USER"))
+                 .authorizeHttpRequests(ar->ar.requestMatchers("/admin/**").hasRole("ADMIN"))
+                 .authorizeHttpRequests(ar->ar.requestMatchers("/public/**").permitAll())
                  .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
                  .build();
     }
--- src/main/java/ma/enset/glsid_spring_mvc/web/ProductController.java
+++ src/main/java/ma/enset/glsid_spring_mvc/web/ProductController.java
@@ -21,7 +21,7 @@ public class ProductController {
 
     private ProductRepository productRepository;
 
-    @GetMapping("/index")
+    @GetMapping("/user/index")
     public String index(Model model){
         List<Product> products = productRepository.findAll();
         model.addAttribute("productList",products);
@@ -30,26 +30,26 @@ public class ProductController {
 
     @GetMapping("/")
     public String Home(){
-        return "redirect:/index";
+        return "redirect:/user/index";
     }
 
-    @GetMapping("/newProduct")
+    @GetMapping("/admin/newProduct")
     public String NewProduct(Model model){
         model.addAttribute("product",new Product());
         return "new-product";
     }
 
-    @GetMapping("/delete")
+    @GetMapping("/admin/delete")
     public String Delete(@RequestParam(name = "id") Long id){
         productRepository.deleteById(id);
-        return "redirect:/index";
+        return "redirect:/user/index";
     }
 
-    @PostMapping("/saveProduct")
+    @PostMapping("/admin/saveProduct")
     public String SaveProduct(@Valid Product product, BindingResult bindingResult,Model model){
         if (bindingResult.hasErrors()) return "new-product";
         productRepository.save(product);
-        return "redirect:/newProduct";
+        return "redirect:/admin/newProduct";
     }
 
 }
